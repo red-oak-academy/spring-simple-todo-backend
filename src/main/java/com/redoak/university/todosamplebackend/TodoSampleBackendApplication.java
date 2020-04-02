7f6e027a7bb3ccd80a8ce05cc13b46b2dc89a974
Robin Wloka <robin.wloka@red-oak-consulting.com>
2020-04-02 17:10:02 +0200

    Initializre Project added.

---
+++ src/main/java/com/redoak/university/todosamplebackend/TodoSampleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.redoak.university.todosamplebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSampleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSampleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redoak/university/todosamplebackend/TodoSampleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redoak.university.todosamplebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSampleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
